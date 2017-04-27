637d91c4e50308a4d6cdb2a3abd160d78705ef52
ColdEsper <ccopelan@uci.edu>
2017-04-26 20:32:43 -0700

    Price field in games table in new version of data.sql and createtable.sql. Older versions renamed by adding _v1 to file name.

--- project1_63/createtable.sql
+++ project1_63/createtable.sql
@@ -1,31 +1,32 @@
+CREATE TABLE publishers (
+   id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+   publisher VARCHAR(200)
+);
+
 CREATE TABLE platforms (
    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   platform VARCHAR(50) NOT NULL
+   platform VARCHAR(200)
 );
 
 CREATE TABLE games (
    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
    rank INTEGER,
-   name VARCHAR(200) NOT NULL,
-   year YEAR NOT NULL,
-   globalsales VARCHAR(20)
+   name VARCHAR(200),
+   year YEAR,
+   globalsales VARCHAR(200),
+   price INTEGER
 );
 
 CREATE TABLE genres (
    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   genre VARCHAR(50) NOT NULL
+   genre VARCHAR(200)
 );
 
-CREATE TABLE publishers (
-   id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   publisher VARCHAR(200) NOT NULL
-);
-
-CREATE TABLE platforms_of_games (
+CREATE TABLE genres_of_games (
    game_id INTEGER NOT NULL,
-   platform_id INTEGER NOT NULL,
-   CONSTRAINT fk_platforms_of_games_games FOREIGN KEY (`game_id`) REFERENCES `games`(id) ON DELETE CASCADE,
-   CONSTRAINT fk_platforms_of_games_platforms FOREIGN KEY (`platform_id`) REFERENCES `platforms`(id) ON DELETE CASCADE
+   genre_id INTEGER NOT NULL,
+   CONSTRAINT fk_genres_of_games_games FOREIGN KEY (`game_id`) REFERENCES `games`(id) ON DELETE CASCADE,
+   CONSTRAINT fk_genres_of_games_genres FOREIGN KEY (`genre_id`) REFERENCES `genres`(id) ON DELETE CASCADE
 );
 
 CREATE TABLE publishers_of_games (
@@ -37,28 +38,28 @@ CREATE TABLE publishers_of_games (
    CONSTRAINT fk_publishers_of_games_platforms FOREIGN KEY (`platform_id`) REFERENCES `platforms`(id) ON DELETE CASCADE
 );
 
-CREATE TABLE genres_of_games (
+CREATE TABLE platforms_of_games (
    game_id INTEGER NOT NULL,
-   genre_id INTEGER NOT NULL,
-   CONSTRAINT fk_genres_of_games_games FOREIGN KEY (`game_id`) REFERENCES `games`(id) ON DELETE CASCADE,
-   CONSTRAINT fk_genres_of_games_genres FOREIGN KEY (`genre_id`) REFERENCES `genres`(id) ON DELETE CASCADE
+   platform_id INTEGER NOT NULL,
+   CONSTRAINT fk_platforms_of_games_games FOREIGN KEY (`game_id`) REFERENCES `games`(id) ON DELETE CASCADE,
+   CONSTRAINT fk_platforms_of_games_platforms FOREIGN KEY (`platform_id`) REFERENCES `platforms`(id) ON DELETE CASCADE
 );
 
 CREATE TABLE creditcards (
-   id VARCHAR(20) PRIMARY KEY NOT NULL,
-   first_name VARCHAR(50) NOT NULL,
-   last_name VARCHAR(50) NOT NULL,
+   id VARCHAR(200) PRIMARY KEY NOT NULL,
+   first_name VARCHAR(200) NOT NULL,
+   last_name VARCHAR(200) NOT NULL,
    expiration DATE NOT NULL
 );
 
 CREATE TABLE customers (
    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-   cc_id VARCHAR(20) NOT NULL,
-   first_name VARCHAR(50) NOT NULL,
-   last_name VARCHAR(50) NOT NULL,
+   cc_id VARCHAR(200) NOT NULL,
+   first_name VARCHAR(200) NOT NULL,
+   last_name VARCHAR(200) NOT NULL,
    address VARCHAR(200) NOT NULL,
-   email VARCHAR(50) NOT NULL,
-   password VARCHAR(20) NOT NULL,
+   email VARCHAR(200) NOT NULL,
+   password VARCHAR(200) NOT NULL,
    CONSTRAINT fk_customers_creditcards FOREIGN KEY (`cc_id`) REFERENCES `creditcards`(id) ON DELETE CASCADE
 );
 
