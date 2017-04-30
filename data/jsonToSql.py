9b0caa81b30afabe91a5957ee96d525b8a9cad26
ColdEsper <ccopelan@uci.edu>
2017-04-30 01:49:50 -0700

    Many more game trailers added.

--- data/jsonToSql.py
+++ data/jsonToSql.py
@@ -1,9 +1,9 @@
 import json
 
 #configuration options
-FILE_NAME= "game_logos.json"
+FILE_NAME= "trailers.json"
 TABLE = "games"
-NEW_COLUMN = "logo"
+NEW_COLUMN = "url"
 NEW_COLUMN_TYPE=NEW_COLUMN+" VARCHAR(511)"
 JSON_KEY="name"
 
@@ -12,11 +12,10 @@ data = None
 with open(FILE_NAME) as jfile:
     data = json.load(jfile)
 
-print(data["Doom 3"])
-
-#if data!=None:
-#    with open(FILE_NAME+".sql","w") as sql:
-#        sql.write("ALTER TABLE "+TABLE+"\n   ADD "+NEW_COLUMN_TYPE+";\n")
-#        for key in data.keys():
-#            sql.write("UPDATE "+TABLE+"\n   SET "+NEW_COLUMN+" = '"+data[key]
-#                    +"'\n   WHERE "+JSON_KEY+" = '"+key.replace("'","\\'")+"';\n")
+if data!=None:
+    with open(FILE_NAME+".sql","w") as sql:
+        sql.write("ALTER TABLE "+TABLE+"\n   ADD "+NEW_COLUMN_TYPE+";\n")
+        for key in data.keys():
+            if len(data[key]) > 0:
+                sql.write("UPDATE "+TABLE+"\n   SET "+NEW_COLUMN+" = 'www.youtube.com/watch?v="+data[key][0]
+                    +"'\n   WHERE "+JSON_KEY+" = '"+key.replace("'","\\'")+"';\n")
