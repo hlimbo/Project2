c35eb28296d685af1089cd0f5bb6e98d7e49e96f
ColdEsper <ccopelan@uci.edu>
2017-05-01 10:22:18 -0700

    Removed horribly inefficient masterTable from source.

--- ROOT/WEB-INF/sources/SearchServlet.java
+++ ROOT/WEB-INF/sources/SearchServlet.java
@@ -219,16 +219,10 @@ public class SearchServlet extends HttpServlet
                 searchAll = true;
             }
             if (!searchAll) {
-                /*String masterTable = "((SELECT id AS game_id FROM games) AS g_id NATURAL JOIN "
-                    +"publishers_of_games NATURAL JOIN genres_of_games NATURAL JOIN (SELECT id AS publisher_id FROM publishers) AS p_id "
-                    +"NATURAL JOIN (SELECT id AS genre_id FROM genres) AS n_id NATURAL JOIN (SELECT id AS platform_id FROM platforms) AS l_id"
-                    +" NATURAL JOIN platforms_of_games)";*/
                 String masterTable = "platforms_of_games NATURAL JOIN genres_of_games NATURAL JOIN publishers_of_games";
                 //duplicates due to games on multiple platforms, with multiple genres, or etc...
                 query = "SELECT DISTINCT "+table+".* FROM games, publishers, platforms, genres, "+masterTable+" WHERE "
                     +"games.id=game_id AND publishers.id=publisher_id AND platforms.id=platform_id";
-                /*query = "SELECT DISTINCT "+table+".* FROM games, publishers, platforms, genres, "+
-                    +" WHERE games.id=game_id AND publishers.id=publisher_id AND platforms.id=platform_id";*/
                 query+=addSearchTerm(request,"name",useSubMatch);
                 query+=addSearchTerm(request,"year",useSubMatch);
                 query+=addSearchTerm(request,"publisher",useSubMatch);
